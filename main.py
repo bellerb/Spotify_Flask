303c5bc93a5492bb3365665fe9b49b2e865c6f1b
Ben <26092263+bellerb@users.noreply.github.com>
2017-08-18 21:29:05 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+#**********************************************************************************
+#Spotify_Handle V0.01
+#By:Ben Bellerose
+#Description: This is the routing for spotify example.
+#**********************************************************************************
+from flask import Flask, request, redirect, g, render_template
+from spotify import app_Authorization, user_Authorization, Album_Data, Profile_Data, Playlist_Data
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # Authorization
+    auth_url = app_Authorization()
+    return redirect(auth_url)
+
+@app.route("/callback/q")
+def callback():
+    authorization_header = user_Authorization()
+
+    #Gathering of profile data
+    profile_data = Profile_Data(authorization_header)
+    Name = profile_data["display_name"]
+    external_urls = profile_data["external_urls"]
+    uri = profile_data["uri"]
+    href = profile_data["href"]
+    followers = profile_data["followers"]["total"]
+    images = profile_data["images"]
+
+    #Gathering of playlist data
+    playlist_data = Playlist_Data(authorization_header,profile_data)
+    x = 0
+    playlist_url = []
+    playlist_tracks = []
+    playlist_titles = []
+    while x < len(playlist_data["items"]):
+        playlist_tracks.insert(len(playlist_tracks),playlist_data["items"][x]["tracks"]['total'])
+        playlist_url.insert(len(playlist_url),playlist_data["items"][x]["tracks"]['href'])
+        playlist_titles.insert(len(playlist_titles),playlist_data["items"][x]["name"])
+        x = x + 1
+
+    #Gathering of album data
+    artist_data = Album_Data(authorization_header,profile_data)
+    x = 0
+    album_titles = []
+    album_uri = []
+    album_label = []
+    album_tracknames = []
+    album_trackartist = []
+    while x < len(artist_data["items"]):
+        b = 0
+        while b < len(artist_data["items"][x]["album"]["tracks"]['items']):
+            album_titles.insert(len(album_titles),artist_data["items"][x]["album"]["name"])
+            album_uri.insert(len(album_uri),artist_data["items"][x]["album"]["uri"])
+            album_label.insert(len(album_label),artist_data["items"][x]["album"]["label"])
+            album_tracknames.insert(len(album_tracknames),artist_data["items"][x]["album"]["tracks"]['items'][b]["name"])
+            album_trackartist.insert(len(album_trackartist),artist_data["items"][x]["album"]["tracks"]['items'][b]["artists"][0]['name'])
+            b = b + 2
+        x = x + 1
+    track_hold = {'artist': album_trackartist, 'titles': album_tracknames, 'album': album_titles, 'label': album_label}
+    track_hold = zip(track_hold['artist'],track_hold['titles'], track_hold['album'],track_hold['label'])
+
+    # Combine profile and playlist data to display
+    display_arr = [profile_data] + [playlist_data["items"][0]] + [artist_data["items"][0]["album"]]
+    return render_template("songbank.html", x = track_hold)
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=8080)
---
+++ spotify.py
@@ -0,0 +1,92 @@
+#**********************************************************************************
+#Spotify_Handle V0.01
+#By:Ben Bellerose
+#Description: This program handles all spotify interaction for the application.
+#Reference: https://github.com/drshrey/spotify-flask-auth-example
+#**********************************************************************************
+import json
+import requests
+import base64
+import urllib
+from flask import request
+
+# Client Keys
+CLIENT_ID = "<FILL IN YOUR CLIENT ID>"
+CLIENT_SECRET = "<FILL IN YOUR CLIENT SECRET>"
+
+#Spotify URLS
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+
+#Server-side Parameters
+CLIENT_SIDE_URL = "http://127.0.0.1"
+PORT = 8080
+REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
+SCOPE = "user-library-read"
+STATE = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+#Authorization of application with spotify
+def app_Authorization():
+    auth_query_parameters = {
+        "response_type": "code",
+        "redirect_uri": REDIRECT_URI,
+        "scope": SCOPE,
+        # "state": STATE,
+        # "show_dialog": SHOW_DIALOG_str,
+        "client_id": CLIENT_ID
+    }
+    url_args = "&".join(["{}={}".format(key,urllib.quote(val)) for key,val in auth_query_parameters.iteritems()])
+    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+    return auth_url
+
+#User allows us to acces there spotify
+def user_Authorization():
+    auth_token = request.args['code']
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": REDIRECT_URI
+    }
+    base64encoded = base64.b64encode("{}:{}".format(CLIENT_ID, CLIENT_SECRET))
+    headers = {"Authorization": "Basic {}".format(base64encoded)}
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers)
+
+    # Tokens are Returned to Application
+    response_data = json.loads(post_request.text)
+    access_token = response_data["access_token"]
+    refresh_token = response_data["refresh_token"]
+    token_type = response_data["token_type"]
+    expires_in = response_data["expires_in"]
+
+    # Use the access token to access Spotify API
+    authorization_header = {"Authorization":"Bearer {}".format(access_token)}
+    return authorization_header
+
+#Gathering of profile information
+def Profile_Data(header):
+    # Get user profile data
+    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
+    profile_response = requests.get(user_profile_api_endpoint, headers=header)
+    profile_data = json.loads(profile_response.text)
+    return profile_data
+
+#Gathering of playlist information
+def Playlist_Data(header,profile):
+    # Get user playlist data
+    playlist_api_endpoint = "{}/playlists".format(profile["href"])
+    playlists_response = requests.get(playlist_api_endpoint, headers=header)
+    playlist_data = json.loads(playlists_response.text)
+    return playlist_data
+
+#Gathering of album information
+def Album_Data(header,profile):
+    # Get user albums data
+    artist_api_endpoint = "{}/albums".format(profile["href"])
+    artist_response = requests.get(artist_api_endpoint, headers=header)
+    artist_data = json.loads(artist_response.text)
+    return artist_data
